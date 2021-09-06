8fed825807b770d1906f15fb017f5c96cbe37381
septem <qitengyuxi@gmail.com>
2021-09-07 00:29:33 +0900

    タスク新規作成画面、編集画面作成

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -11,4 +11,14 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get new" do
+    get tasks_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get tasks_edit_url
+    assert_response :success
+  end
+
 end
