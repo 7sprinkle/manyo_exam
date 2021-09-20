23ea4a28bfada80a464d00ad3857f3caf0117af6
septem <qitengyuxi@gmail.com>
2021-09-20 18:54:09 +0900

    seedデータ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
      if @user.save
-#      session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id), notice:"登録が完了しました！"
 # #       redirect_to tasks_path, notice:"登録が完了しました！"
      else
